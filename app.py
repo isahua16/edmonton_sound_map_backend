423995ecf39ae20cddf480c00681c3c9f6788bbe
isahua16 <isaelhuard@gmail.com>
2023-07-05 15:04:54 -0600

    created user sign up endpoint

--- app.py
+++ app.py
@@ -4,6 +4,29 @@ from apihelpers import check_data, save_file
 from flask import Flask, request, make_response, jsonify, send_from_directory
 import uuid
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 50 * 1000000
+
+@app.post('/api/user')
+def post_user():
+    error = check_data(request.form, ['email', 'username', 'password'])
+    if(error != None):
+        return make_response(jsonify(error), 400)
+    error = check_data(request.files, ['image'])
+    if(error != None):
+        return make_response(jsonify(error), 400)
+    if(request.content_length < (0.5 * 1000000)):
+        filename = save_file(request.files['image'])
+    if(filename == None):
+        return make_response(jsonify("Something has gone wrong"), 500)
+    token = uuid.uuid4().hex
+    salt = uuid.uuid4().hex
+    results = run_statement('call post_user(?,?,?,?,?,?,?)', [request.form.get('email'), request.form.get('username'), filename, request.form.get('bio'), request.form.get('password'), token, salt])
+    if(type(results) == list):
+        return make_response(jsonify(results), 200)
+    else:
+        return make_response("Something went wrong", 500)
+
+
 if(production_mode == True):
     print('Running in production mode')
     import bjoern # type: ignore
--- dbhelpers.py
+++ dbhelpers.py
@@ -1,27 +1,6 @@
 import mariadb
 import dbcreds
-import os
-import uuid
-def save_file(file):
-    # Check to see if first, the filename contains a . character. 
-    # Then, split the filename around the . characters into an array
-    # Then, see if the filename ends with any of the given extensions in the array
-    # You can add or remove file types you want or do not want the user to store
-    if('.' in file.filename and file.filename.rsplit('.', 1)[1].lower() in ['gif','png','jpg','jpeg', 'webp', 'pdf']):
-        # Create a new filename with a token so we don't get duplicate file names
-        # End the filename with . and the original filename extension
-        filename = uuid.uuid4().hex + '.' + file.filename.rsplit('.', 1)[1].lower()
-        try:
-            # Use built-in functions to save the file in the images folder
-            # You can put any path you want, in my example I just need them in the images folder right here
-            file.save(os.path.join('images', filename))
-            # Return the filename so it can be stored in the DB
-            return filename
-        except Exception as error:
-            # If something goes wrong, print out to the terminal and return nothing
-            print(FILE SAVE ERROR: , error)
-            return error.msg
-    # If any conditional is not met or an error occurs, None is returned
+
 def convert_data(cursor, results):
     column_names = [i[0] for i in cursor.description]
     new_results = []
